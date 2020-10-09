eb560b71c6bb55cba50577034f495d5593f4013a
acxz <17132214+acxz@users.noreply.github.com>
2020-10-08 21:25:56 -0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='gym-continuous-classic-control',
+    version='0.0.1',
+    author='acxz',
+    long_description='',
+    description='',
+    zip_safe=False,
+    packages=['gym_continuous_classic_control'],
+    install_requires=[
+        "gym",
+        "numpy",
+    ],
+)
