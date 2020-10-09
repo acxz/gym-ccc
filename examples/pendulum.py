245f13fa7597f66d795842b914f6373e0e030643
acxz <17132214+acxz@users.noreply.github.com>
2020-10-08 22:21:45 -0400

    add example usage for pendulum env

---
+++ examples/pendulum.py
@@ -0,0 +1,13 @@
+import gym
+import numpy as np
+
+def main():
+    env = gym.make('gym_continuous_classic_control.envs:ContinuousPendulum-v0')
+
+    obs = env.reset()
+    while True:
+        obs, reward, done, info = env.step(np.array([0]))
+        env.render('human')
+
+if __name__ == "__main__":
+    main()
