42adf9ed7f8d3fb7d4c83abf1667241fb8644bda
acxz <17132214+acxz@users.noreply.github.com>
2020-11-06 14:43:37 -0500

    rearrage envs

--- gym_ccc/envs/__init__.py
+++ gym_ccc/envs/__init__.py
@@ -5,40 +5,13 @@ from gym_ccc.envs.cont_car import ContCarEnv  # noqa: F401
 from gym_ccc.envs.cont_car import NonNormContCarEnv  # noqa: F401
 from gym_ccc.envs.cont_cartpole import ContCartPoleEnv  # noqa: F401
 from gym_ccc.envs.cont_cartpole import NonNormContCartPoleEnv  # noqa: F401
+from gym_ccc.envs.cont_multirotor import NonNormContMultirotorEnv  # noqa: F401
 from gym_ccc.envs.cont_pendulum import ContPendulumEnv  # noqa: F401
 from gym_ccc.envs.cont_pendulum import NonNormContPendulumEnv  # noqa: F401
 
-
 register(
-    id='NonNormContPendulum-v0',
-    entry_point='gym_ccc.envs:NonNormContPendulumEnv',
-    reward_threshold=None,
-    nondeterministic=False,
-    max_episode_steps=None,
-    kwargs={},
-)
-
-register(
-    id='ContPendulum-v0',
-    entry_point='gym_ccc.envs:ContPendulumEnv',
-    reward_threshold=None,
-    nondeterministic=False,
-    max_episode_steps=None,
-    kwargs={},
-)
-
-register(
-    id='NonNormContCartPole-v0',
-    entry_point='gym_ccc.envs:NonNormContCartPoleEnv',
-    reward_threshold=None,
-    nondeterministic=False,
-    max_episode_steps=None,
-    kwargs={},
-)
-
-register(
-    id='ContCartPole-v0',
-    entry_point='gym_ccc.envs:ContCartPoleEnv',
+    id='ContCar-v0',
+    entry_point='gym_ccc.envs:ContCarEnv',
     reward_threshold=None,
     nondeterministic=False,
     max_episode_steps=None,
@@ -54,9 +27,38 @@ register(
     kwargs={},
 )
 
+
 register(
-    id='ContCar-v0',
-    entry_point='gym_ccc.envs:ContCarEnv',
+    id='ContCartPole-v0',
+    entry_point='gym_ccc.envs:ContCartPoleEnv',
+    reward_threshold=None,
+    nondeterministic=False,
+    max_episode_steps=None,
+    kwargs={},
+)
+
+register(
+    id='NonNormContCartPole-v0',
+    entry_point='gym_ccc.envs:NonNormContCartPoleEnv',
+    reward_threshold=None,
+    nondeterministic=False,
+    max_episode_steps=None,
+    kwargs={},
+)
+
+
+register(
+    id='ContPendulum-v0',
+    entry_point='gym_ccc.envs:ContPendulumEnv',
+    reward_threshold=None,
+    nondeterministic=False,
+    max_episode_steps=None,
+    kwargs={},
+)
+
+register(
+    id='NonNormContPendulum-v0',
+    entry_point='gym_ccc.envs:NonNormContPendulumEnv',
     reward_threshold=None,
     nondeterministic=False,
     max_episode_steps=None,
