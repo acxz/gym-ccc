4625a011949ba1c15d0fd38fdc1ce4e1e4d8cb54
acxz <17132214+acxz@users.noreply.github.com>
2020-11-12 09:51:25 -0500

    add custom reset, time, cost

--- gym_ccc/envs/cartpole.py
+++ gym_ccc/envs/cartpole.py
@@ -27,14 +27,14 @@ class CartPoleNonNormEnv(classic_control.CartPoleEnv):
         Num   Action                      Min                     Max
         0     Force on the cart           -Inf                    Inf
     Reward:
-        None
+        Quadratic cost from goal state of [0, 0, 0, 0]
     Starting State:
         All observations are assigned a uniform random value in [-0.05..0.05]
     """
 
     # pylint: disable=too-many-arguments
     def __init__(self, gravity=9.8, masscart=1.0, masspole=0.1, polelength=1.0,
-                 tau=0.02):
+                 dt=0.02, custom_reset=None):
         """Init cartpole env."""
         super().__init__()
 
@@ -43,7 +43,12 @@ class CartPoleNonNormEnv(classic_control.CartPoleEnv):
         self.masspole = masspole
         self.polelength = polelength
         self.length = self.polelength/2  # For rendering purposes
-        self.tau = tau
+        self.dt = dt
+
+        self.state = None
+        self.time = 0
+
+        self.custom_reset = custom_reset
 
         action_high = np.array([np.finfo(np.float32).max], dtype=np.float32)
 
@@ -62,6 +67,10 @@ class CartPoleNonNormEnv(classic_control.CartPoleEnv):
         """Propagate dynamics."""
         pos, pos_dot, theta, theta_dot = self.state
         force = action
+        self.time += self.dt
+
+        cost = pos ** 2 + pos_dot ** 2 + theta ** 2 + theta_dot ** 2
+
         costheta = np.cos(theta)
         sintheta = np.sin(theta)
 
@@ -71,27 +80,37 @@ class CartPoleNonNormEnv(classic_control.CartPoleEnv):
                              self.gravity * costheta)
                             + force)
 
-        thetaacc = (1/(self.polelength * temp) *
-                    (- self.masspole * self.polelength * theta_dot**2 *
-                        costheta * sintheta -
+        theta_acc = (1/(self.polelength * temp) *
+                     (- self.masspole * self.polelength * theta_dot**2 *
+                      costheta * sintheta -
                      (self.masscart + self.masspole) * self.gravity *
                      sintheta -
                      force * costheta))
 
-        pos = pos + self.tau * pos_dot
-        pos_dot = pos_dot + self.tau * pos_acc
-        theta = theta + self.tau * theta_dot
-        theta_dot = theta_dot + self.tau * thetaacc
+        pos = pos + self.dt * pos_dot
+        pos_dot = pos_dot + self.dt * pos_acc
+        theta = theta + self.dt * theta_dot
+        theta_dot = theta_dot + self.dt * theta_acc
 
-        theta = angle_normalize(theta)
-        self.state = (pos, pos_dot, theta, theta_dot)
+        theta = self.angle_normalize(theta)
+        self.state = np.array([pos, pos_dot, theta, theta_dot])
 
-        return np.array(self.state), 0, False, {}
+        return self.state, -cost, False, {'time': self.time}
 
     def reset(self):
-        """Reset to a random state."""
-        self.state = self.np_random.uniform(low=-0.05, high=0.05, size=(4,))
-        return np.array(self.state)
+        """Reset environment."""
+        self.time = 0
+        if self.custom_reset is not None:
+            self.state = self.custom_reset()
+        else:
+            self.state = \
+                self.np_random.uniform(low=-0.05, high=0.05, size=(4,))
+        return self.state, 0, False, {'time': self.time}
+
+    @staticmethod
+    def angle_normalize(ang):
+        """Normalize angle between -pi and pi."""
+        return (((ang+np.pi) % (2*np.pi)) - np.pi)
 
 
 class CartPoleEnv(CartPoleNonNormEnv):
@@ -101,17 +120,26 @@ class CartPoleEnv(CartPoleNonNormEnv):
         """Init."""
         super().__init__(**kwargs)
 
+        obs_high = np.array([np.finfo(np.float32).max,
+                             np.finfo(np.float32).max,
+                             1,
+                             np.finfo(np.float32).max],
+                            dtype=np.float32)
+
+        self.observation_space = spaces.Box(-obs_high,
+                                            obs_high, dtype=np.float32)
+
     def step(self, action):
         """Propagate dynamics forward and keep track of state in info."""
         _, reward, done, info = super().step(action)
-        info['state'] = np.array(self.state)
+        info['state'] = self.state
 
         return self._get_obs(), reward, done, info
 
     def reset(self):
         """Reset to a random observation."""
-        self.state = super().reset()
-        return self._get_obs()
+        self.state, reward, done, info = super().reset()
+        return self._get_obs(), reward, done, info
 
     def _get_obs(self):
         pos, pos_dot, theta, theta_dot = self.state
@@ -119,6 +147,3 @@ class CartPoleEnv(CartPoleNonNormEnv):
                          theta_dot])
 
 
-def angle_normalize(ang):
-    """Normalize angle between -pi and pi."""
-    return (((ang+np.pi) % (2*np.pi)) - np.pi)
