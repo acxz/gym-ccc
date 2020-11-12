c52e2ccf8d62170b8ee99718be1399938ce34a77
acxz <17132214+acxz@users.noreply.github.com>
2020-11-12 08:16:45 -0500

    add custom reset, time, and dynamics clipping

--- gym_ccc/envs/pendulum.py
+++ gym_ccc/envs/pendulum.py
@@ -11,13 +11,23 @@ class PendulumNonNormEnv(classic_control.PendulumEnv):
 
     # pylint: disable=too-many-arguments
     # pylint: disable=super-init-not-called
-    def __init__(self, gravity=10.0, mass=1, length=1, damping=1, dt=0.05):
+    def __init__(self, gravity=10.0, mass=1, length=1, damping=0.1, dt=0.05,
+                 max_speed=8, max_torque=2, custom_reset=None):
         """Init."""
+        self.max_speed = max_speed
+        self.max_torque = max_torque
         self.dt = dt
-        self.g = gravity
-        self.m = mass
+        self.gravity = gravity
+        self.mass = mass
         self.length = length
         self.damping = damping
+
+        self.state = None
+        self.time = 0
+
+        self.custom_reset = custom_reset
+
+        self.last_u = None
         self.viewer = None
 
         self.action_space = spaces.Box(
@@ -27,7 +37,7 @@ class PendulumNonNormEnv(classic_control.PendulumEnv):
         )
         self.observation_space = spaces.Box(
             low=np.array([-np.pi, -np.inf], dtype=np.float32),
-            high=-np.array([np.pi, np.inf], dtype=np.float32),
+            high=np.array([np.pi, np.inf], dtype=np.float32),
             dtype=np.float32
         )
 
@@ -35,30 +45,47 @@ class PendulumNonNormEnv(classic_control.PendulumEnv):
 
     def step(self, u):
         """Propagate the dynamics."""
-        th, thdot = self.state  # th := theta
+        theta, theta_dot = self.state
 
-        g = self.g
-        m = self.m
+        gravity = self.gravity
+        mass = self.mass
         length = self.length
         damping = self.damping
+        # pylint: disable=invalid-name
         dt = self.dt
+        self.time += dt
 
-        u = u[0]
-        self.last_u = u  # for rendering
+        torque = np.clip(u, -self.max_torque, self.max_torque)[0]
+        self.last_u = torque  # for rendering
+        costs = self.angle_normalize(theta) ** 2 + 0.1 * theta_dot ** 2 + \
+            0.001 * torque ** 2
 
-        newth = th + thdot * dt
-        newth = angle_normalize(newth)
-        newthdot = thdot + (-g / length * np.sin(th + np.pi) -
-                            (damping * thdot + u) / (m * length ** 2)) * dt
+        new_theta = theta + theta_dot * dt
+        new_theta = self.angle_normalize(new_theta)
+        new_theta_dot = theta_dot + \
+            (-gravity / length * np.sin(theta + np.pi) -
+             (damping * theta_dot + torque) /
+             (mass * length ** 2)) * dt
+        new_theta_dot = np.clip(new_theta_dot, -self.max_speed, self.max_speed)
 
-        self.state = np.array([newth, newthdot])
-        return self.state, 0, False, {}
+        self.state = np.array([new_theta, new_theta_dot])
+        return self.state, -costs, False, {'time': self.time}
 
     def reset(self):
-        """Reset environment to random initial condition."""
-        high = np.array([np.pi, 1])
-        self.state = self.np_random.uniform(low=-high, high=high)
-        return self.state
+        """Reset environment."""
+        self.time = 0
+        if self.custom_reset is not None:
+            self.state = self.custom_reset()
+        else:
+            high = np.array([np.pi, 1])
+            self.state = self.np_random.uniform(low=-high, high=high)
+        self.last_u = None
+        return self.state, 0, False, {'time': self.time}
+
+    @staticmethod
+    def angle_normalize(angle):
+        """Ensure angle is between -pi and pi."""
+        return ((angle+np.pi) % (2*np.pi)) - np.pi
 
 
 class PendulumEnv(PendulumNonNormEnv):
@@ -70,17 +97,13 @@ class PendulumEnv(PendulumNonNormEnv):
 
     def step(self, u):
         """Add state to the info dict."""
-        obs, reward, done, info = super().step(u)
+        _, reward, done, info = super().step(u)
         info['state'] = self.state
 
         return self._get_obs(), reward, done, info
 
     def reset(self):
         """Reset environment according to super class."""
-        self.state = super().reset()
-        return self._get_obs()
-
-
-def angle_normalize(x):
-    """Ensure angle is between -pi and pi."""
-    return (((x+np.pi) % (2*np.pi)) - np.pi)
+        _, reward, done, info = super().reset()
+        info['state'] = self.state
+        return self._get_obs(), reward, done, info
